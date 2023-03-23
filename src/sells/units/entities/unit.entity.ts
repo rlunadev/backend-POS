b4fdc53ea3539fa3a37a494a94ef61a606ac15d1
rlunadev <ronaldwin79@hotmail.com>
2023-03-23 07:41:10 -0400

    add table unit, role, user

--- src/sells/units/entities/unit.entity.ts
+++ src/sells/units/entities/unit.entity.ts
@@ -1,1 +1,20 @@
-export class Unit {}
+import { BaseEntity } from 'src/shared/persistence/base.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('units')
+export class Unit extends BaseEntity{
+
+    @PrimaryGeneratedColumn('uuid', { name: 'id' })
+    id: number;
+  
+    @Column('varchar', {
+        length: 50,
+        unique: true,
+        name: 'name',
+      })
+    name: string;
+  
+    @Column('varchar', {length: 50})
+    description: string;
+
+}
--- src/users/users/entities/user.entity.ts
+++ src/users/users/entities/user.entity.ts
@@ -1,1 +1,20 @@
-export class User {}
+import { BaseEntity } from 'src/shared/persistence/base.entity';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('users')
+export class User extends BaseEntity{
+
+    @PrimaryGeneratedColumn('uuid', { name: 'id' })
+    id: number;
+  
+    @Column('varchar', {
+        length: 50,
+        unique: true,
+        name: 'email',
+      })
+    name: string;
+  
+    @Column({ name: 'password_hash' })
+    passwordHash: string;
+
+}
